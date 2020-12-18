eb11641099f30edab934c4460ac1bb727dc765ed
mohak3521 <mohak_k@ph.iitr.ac.in>
2020-12-18 21:24:09 +0530

    update

--- scwebsite/shloka.py
+++ scwebsite/shloka.py
@@ -28,18 +28,19 @@ s = {'shloka':
       पाण्डवाः हर्षनिर्भराः The pandavas (a species of water-birds) are filled with joy,<br>रुदन्ति कौ-रवाः तत्र And hence the crows (कौ-रव the ones who have voices like 'kau') are crying.<br>
       हा हा केशव केशव Ha ha Keshav!! What a miracle!'''),
       
-      ('Contradictory Meaning',
-        '''केशवं पतितं दृष्ट्वा</br>
-            पाण्डवाः हर्षनिर्भराः |</br>
-            रुदन्ति कौरवास्तत्र</br>
-            हा हा केशव केशव ||''',
-        'https://www.dropbox.com/s/svkg5yc56dlcy46/keshav.mp4?raw=1',
-        '''Here, the poet creates an illusion by leading the reader to believe what appears to be <i>prima facie</i> in the subhashitam.<br>
-      So translated literally, it says that the Pandavas are overjoyed on seeing Keshav (Lord Krishna) fallen down, while the Kauravas
-      are crying! This is somewhat contradictory to the common sense, as Krishna was on the side of Pandavas and against Kauravas,
-      in the great war of Mahabharat. But the poet has tactfully used pun, and the other meaning is amusing indeed.<br>The verse is composed in anuṣṭup (अनुष्टुप्) meter.<br><br>के शवं पतितं दृष्ट्वा On seeing the dead body (शवं) fallen, in the water (के),<br>
-      पाण्डवाः हर्षनिर्भराः The pandavas (a species of water-birds) are filled with joy,<br>रुदन्ति कौ-रवाः तत्र And hence the crows (कौ-रव the ones who have voices like 'kau') are crying.<br>
-      हा हा केशव केशव Ha ha Keshav!! What a miracle!'''),
+      ('Verse from a sound',
+        '''राज्याभिषेके मदविह्वलायाः</br>
+            हस्ताच्च्युतोS हेमघटस्तरुण्याः |</br>
+            सोपानमासाद्य करोति शब्दम्</br>
+            ठं ठं ठठं ठं ठठठं ठठं ठः||''',
+        'https://www.dropbox.com/s/iv96016b91u2rva/rajyabhisheke.m4a?raw=1',
+        '''Composed by one of the legendary poets in the history of Sanskrit, this verse is an example of the games of the learned. The revered Kalidasa constructed this beautiful poem out of a meaningless string of sounds, in the competition
+        of Samasya Pooranam (समस्या-पूरणम्), organised by the great King Bhoj.<br> Such competitions were commonly organised by advocates of literature and scholarship, and Raja Bhoj is one of the most reknowned in this respect.<br> In the particular
+        context of this subhashitam, Raja Bhoj gives a seemingly non-sensical line 'tham tham thatham tham thathatham thatham thah' as a challenge for the assembly of intellectuals gathered in his court, and this subhashitam is the clever solution
+        of Kalidasa to it.<br><br> राज्याभिषेके In the grand coronation ceremony of the king,<br> हेम-घटः च्युतः the golden pot fell away<br> मदविह्वलायाः तरुण्याः हस्तात् from the hands of an excited young girl (so excited about the coronation
+        ceremony that she was not in her senses),<br> सोपानम् आसाद्य करोति शब्दम् and upon reaching the stairs produced the sound -<br> ठं ठं ठठं ठं ठठठं ठठं ठः "tham tham thatham tham thathatham thatham thah"!!<br><br> Kalidasa thus imagines
+        capturing the sound made by the pot at each step. But both Kalidasa and Raja Bhoj show how rhythm and harmony can be found even in the events we would otherwise perceive as noise; for we see, the 'meaningless' string of sounds actually
+        follows the melodiuos meter of indravajrā (इन्द्रवज्रा)!!<br> Such were the pastimes of the scholars!'''),
 
       ('Contradictory Meaning',
         '''केशवं पतितं दृष्ट्वा</br>
