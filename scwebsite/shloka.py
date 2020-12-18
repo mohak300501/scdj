0c902e57c327ad07aa9385d644ed22f5ee8d025b
mohak3521 <mohak_k@ph.iitr.ac.in>
2020-12-18 21:31:33 +0530

    update

--- scwebsite/shloka.py
+++ scwebsite/shloka.py
@@ -42,30 +42,44 @@ s = {'shloka':
         capturing the sound made by the pot at each step. But both Kalidasa and Raja Bhoj show how rhythm and harmony can be found even in the events we would otherwise perceive as noise; for we see, the 'meaningless' string of sounds actually
         follows the melodiuos meter of indravajrā (इन्द्रवज्रा)!!<br> Such were the pastimes of the scholars!'''),
 
-      ('Contradictory Meaning',
-        '''केशवं पतितं दृष्ट्वा</br>
-            पाण्डवाः हर्षनिर्भराः |</br>
-            रुदन्ति कौरवास्तत्र</br>
-            हा हा केशव केशव ||''',
-        'https://www.dropbox.com/s/svkg5yc56dlcy46/keshav.mp4?raw=1',
-        '''Here, the poet creates an illusion by leading the reader to believe what appears to be <i>prima facie</i> in the subhashitam.<br>
-      So translated literally, it says that the Pandavas are overjoyed on seeing Keshav (Lord Krishna) fallen down, while the Kauravas
-      are crying! This is somewhat contradictory to the common sense, as Krishna was on the side of Pandavas and against Kauravas,
-      in the great war of Mahabharat. But the poet has tactfully used pun, and the other meaning is amusing indeed.<br>The verse is composed in anuṣṭup (अनुष्टुप्) meter.<br><br>के शवं पतितं दृष्ट्वा On seeing the dead body (शवं) fallen, in the water (के),<br>
-      पाण्डवाः हर्षनिर्भराः The pandavas (a species of water-birds) are filled with joy,<br>रुदन्ति कौ-रवाः तत्र And hence the crows (कौ-रव the ones who have voices like 'kau') are crying.<br>
-      हा हा केशव केशव Ha ha Keshav!! What a miracle!'''),
+      ('Synonyms',
+        '''अम्बा कुप्यति तात मूर्ध्नि विधृता गङ्गेयमुत्सृज्य ताम्</br>
+            विद्वन् षण्मुख सन्ततम् मयि रता तस्याः गतिः का वद |</br>
+            कोपाटोपवशाद् विवृद्धवदनः प्रत्युत्तरम् दत्तवान्</br>
+            अम्बोधिर्जलधिर्पयोधिरुधधिर् वारांनिधिर्वारिधिः ||''',
+        'https://www.dropbox.com/s/95sgh4ovr4iwznp/amba.ogg?raw=1',
+        '''This shloka reveals the grandeur of Sanskrit language through the immense wealth of wisdom filled within each word of it. The richness of the language is evident from the number of synonyms a word has, wherein each synonym has its
+        own purpose.<br> It's a fact that, in Sanskrit, there is no such thing as a definition for a word; but, rather a word for a definition. Any particular word has its definition hidden within its own self, sometimes conveying deeper meaning
+        than seems to be.<br> Apparaently, this shloka too is part of a Samasya Pooranam, in which six synonyms of ocean are given as the last/problem line. These six words are actually <a>derivatives</a> of the synonyms of water, for eg., जल
+        is water, and adding धि to it, it becomes reservoir of water. Likewise, adding ज to it, जलज means lotus; and adding द, जलद means water-giver (cloud).<br> The poets cleverly exhibits his knowledge of the Puranas along with his intellect,
+        by showing a conversation between Lord Shiva and his son, Lord Kartik. The poet also skillfully makes use of the Śārdūlavikrīḍitam (शार्दूलविक्रीडितम्) meter.<br><br> तात, अम्बा कुप्यति "O Father, mother is angry,"<br> मूर्ध्नि विधृता
+        गङ्गा इयम्, उत्सृज्य ताम् "Throw away the Ganga who is held atop your head (due to whom mother is angry)."<br> विद्वन् षण्मुख "O my learned six-headed,"<br> सन्ततम् मयि रता "The one who stays on me continuously,"<br> तस्याः गतिः का वद
+        "Tell me, where will she go?" (literally, what will be her pace; but as usual, the figurative meaning is sensible)<br> कोप-आटोप-वशाद् विवृद्ध-वदनः The one whose face was swelled by anger and pride (Lord Kartik)<br> प्रत्युत्तरम् दत्तवान्
+        gave a back-answer,<br> अम्बोधिर्जलधिर्पयोधिरुधधिर् वारांनिधिर्वारिधिः "Ambodhi Jaladhi Payodhi Udhadhi Varamnidhi Varidhi!!!" (Six words through six mouths conveying same meaning - to the ocean!)'''),
 
-      ('Contradictory Meaning',
-        '''केशवं पतितं दृष्ट्वा</br>
-            पाण्डवाः हर्षनिर्भराः |</br>
-            रुदन्ति कौरवास्तत्र</br>
-            हा हा केशव केशव ||''',
-        'https://www.dropbox.com/s/svkg5yc56dlcy46/keshav.mp4?raw=1',
-        '''Here, the poet creates an illusion by leading the reader to believe what appears to be <i>prima facie</i> in the subhashitam.<br>
-      So translated literally, it says that the Pandavas are overjoyed on seeing Keshav (Lord Krishna) fallen down, while the Kauravas
-      are crying! This is somewhat contradictory to the common sense, as Krishna was on the side of Pandavas and against Kauravas,
-      in the great war of Mahabharat. But the poet has tactfully used pun, and the other meaning is amusing indeed.<br>The verse is composed in anuṣṭup (अनुष्टुप्) meter.<br><br>के शवं पतितं दृष्ट्वा On seeing the dead body (शवं) fallen, in the water (के),<br>
-      पाण्डवाः हर्षनिर्भराः The pandavas (a species of water-birds) are filled with joy,<br>रुदन्ति कौ-रवाः तत्र And hence the crows (कौ-रव the ones who have voices like 'kau') are crying.<br>
-      हा हा केशव केशव Ha ha Keshav!! What a miracle!''')
+      ('First Shloka in History',
+        '''मा निषाद प्रतिष्ठां त्वम्</br>
+            अगमः शाश्वतिः समाः |</br>
+            यत् क्रॉञ्चमिथुनादेकम्</br>
+            अवधिः काममोहितम् ||''',
+        'https://www.dropbox.com/s/1kvgyl1h3m06qsk/manishad.ogg?raw=1',
+        '''This shloka is actually the result of Maharshi Valmiki's anger!<br> It's said that शोकः श्लोकात्मगतः - Curse took the form of verse!<br> Once when the great sage was returning from his bath, he witnessed a male क्रौञ्च bird - which
+        was engaged in mating - succumbing to the arrow of a hunter. The sage was infuriated by this, and uttered a curse, which miraculously consisted of lines following a fixed rhythm. And thence was born the first shloka in history, along with
+        the legendary meter of anuṣṭup (अनुष्टुप्), that prompted Lord Brahma to order Maharshi Valmiki to compose the epic of Ramayana, in the same meter!<br><br> निषाद "O hunter,"<br> त्वम् प्रतिष्ठां शाश्वतिः समाः मा अगमः "you won't get any
+        prestige or permanentness (in other words, you won't get a place in history),"</br>
+        यत् क्रॉञ्च-मिथुनात् "because, from the union of the Kraunch birds,"</br>
+        एकम् काममोहितम् अवधिः "you have killed one of those infatuated by desire."'''),
+
+      ('Namo Buddhaay',
+        '''कलत्रं च पुत्रं च मित्रादि सर्वम्</br>
+            मनःशान्तये प्राज्यमुत्सृज्य राज्यम् |</br>
+            अपूर्वा त्वया दर्शिता त्यागवृत्तिः</br>
+            नमस्ते महाभाग बुद्ध त्रिवारम् ||''',
+        'https://www.dropbox.com/s/wt3av4z403bcc7w/buddha.mpeg?raw=1',
+        '''उत्सृज्य Giving up<br> कलत्रं च पुत्रं च मित्रादि सर्वम् wife, offspring, friend, and all others,<br> मनःशान्तये प्राज्यम् राज्यम् peace of mind, lavish lifestyle, and the kingdom;<br> अपूर्वा त्वया दर्शिता त्यागवृत्तिः never before
+                was such a sacrificing attitude displayed;<br> नमस्ते महाभाग बुद्ध त्रिवारम् I bow down thrice to you, O great soul of Buddha.<br><br> This shloka is a part of a poem, which has been composed in one of the most perfect and the most symmetric
+                meters, Bhujaṅgaprayātam (भुजङ्गप्रयातम्). Upon inspection, it becomes evident that it follows a fixed pattern of 1 short and 2 long syllables. Represented by |ऽऽ (लघु-गुरु-गुरु), this pattern very strongly resembles the movement of a
+                snake, which travels in one straight direction and performs two curving motions in one cycle. Hence, the name; bhujaṅga means snake, and prayātam means movement.<br> The meter consists of 4 padas (पद, or line), each of which has 4 such
+                cycles of |ऽऽ. The poet takes advantage of this regular rhythm to convey the prayer with reverence, in a soft melodious tune!''')
       }
       }
